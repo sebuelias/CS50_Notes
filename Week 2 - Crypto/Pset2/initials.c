289e45ef4fa9a3bbd50fedd6cb347c0e19e3338f
00saad <00saadchaudhry@gmail.com>
2017-11-19 13:54:14 -0500

    Working on file

--- Week 2 - Crypto/Pset2/initials.c
+++ Week 2 - Crypto/Pset2/initials.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 #include <cs50.h>
 #include <ctype.h>
+#include <string.h>
+
+string getName();
 
 int main(void) {
 
+    string name = getName();
+    printf("%s\n", name);
+
+return 0;
+}
+
+string getName(void) {
+    string name = get_string();
+    return name;
 }
