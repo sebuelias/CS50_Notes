2f848bd2a30e57d182efa1fd9d0572f655cc44dc
00saad <00saadchaudhry@gmail.com>
2017-11-24 20:48:11 -0500

    checking for alphanumeric characters and case

--- Week 2 - Crypto/Pset2/caesar.c
+++ Week 2 - Crypto/Pset2/caesar.c
@@ -8,13 +8,24 @@ int main(int argc, string argv[]) {
 
     if (argc == 2) {
         int key = atoi(argv[1]);
+        printf("plaintext: ");
+        string entry = get_string();
         printf("%i\n", key);
+        if (isalpha(entry[0])) {
+            if(islower(entry[0]))
+            printf("lower\n");
+            else {
+                printf("upper\n");
+            }
+        }
+        /*
         for (char c = 'A'; c <= 'Z'; c++) {
             printf("%c is %i\n", c, ((int) c - 65));
         }
         for (char i = 'a'; i <= 'z'; i++) {
             printf("%c is %i\n", i, ((int) i) - 97);
         }
+        */
     }
     else {
         printf("Please enter one argument\n");
