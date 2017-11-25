602e79018e5de5eeb320cf40d44f37540d3e1464
00saad <00saadchaudhry@gmail.com>
2017-11-24 19:07:05 -0500

    Will return 1 for incorrect argc

--- Week 2 - Crypto/Pset2/caesar.c
+++ Week 2 - Crypto/Pset2/caesar.c
@@ -8,14 +8,11 @@ int main(int argc, string argv[]) {
 
     if (argc == 2) {
         int key = atoi(argv[1]);
-        if (key > 0 && key < 26) {
-            printf("plaintext: ");
-            string plaintext = get_string();
-            printf("%s %i\n", plaintext, key);
-        }
-        else {
-            printf("Please enter a value between 1 and 25\n");
-        }
+        printf("%i\n", key);
+    }
+    else {
+        printf("Please enter one argument\n");
+        return 1;
     }
 
 return 0;
