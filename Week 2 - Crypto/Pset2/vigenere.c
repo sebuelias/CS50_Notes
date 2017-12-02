6dba0097350c09cfbfe4eabae3f4fd6152239ba0
00saad <00saadchaudhry@gmail.com>
2017-12-01 19:01:52 -0500

    Vigenere key messes when it needs to restart

--- Week 2 - Crypto/Pset2/vigenere.c
+++ Week 2 - Crypto/Pset2/vigenere.c
@@ -9,7 +9,7 @@ int main(int argc, string argv[]) {
     if (argc == 2) {
 
         string key = argv[1];
-        int keylen = strlen(key);
+        //int keylen = strlen(key);
 
         printf("plaintext: ");
         string plaintext = get_string();
@@ -17,10 +17,20 @@ int main(int argc, string argv[]) {
         printf("ciphertext: ");
 
         for (int i = 0, n = strlen(plaintext); i < n; i++) {
-            plaintext[i];
-            key[i];
+            //plaintext[i];
+            //key[i];
             //subtract 97 || 65 from both key and plaintext to create alphabet index?
+            if (isalpha(plaintext[i])) {
+                if(islower(plaintext[i])) {
+                    plaintext[i] -= 97;
+                    key[i] -= 97;
+                    plaintext[i] = (plaintext[i] + key[i]) % 26;
+                    plaintext[i] += 97;
+                }
+            }
+            printf("%c", (char)plaintext[i]);
         }
+        printf("\n");
 
 
 
