e826d80279b3d6da3393d1d371c01a9e5e0317b7
00saad <00saadchaudhry@gmail.com>
2017-12-03 13:05:07 -0500

    Got rid of excess code hidden in comments

--- Week 2 - Crypto/Pset2/vigenere.c
+++ Week 2 - Crypto/Pset2/vigenere.c
@@ -17,42 +17,19 @@ int main(int argc, string argv[]) {
         printf("ciphertext: ");
 
         for (int i = 0, n = strlen(plaintext); i < n; i++) {
-            //subtract 97 || 65 from both key and plaintext to create alphabet index?
+            //subtract 97 || 65 from both key and plaintext to create alphabet index
+            string ciphertext = plaintext;
             if (isalpha(plaintext[i])) {
                 if(islower(plaintext[i])) {
                     plaintext[i] -= 97;
                     key[i] -= 97;
                     int j = i % keylen;
-                    plaintext[i] = (plaintext[i] + key[j]) % 26;
-                    plaintext[i] += 97;
+                    ciphertext[i] = (plaintext[i] + key[j]) % 26;
+                    ciphertext[i] += 97;
                 }
             }
-            printf("%c", (char)plaintext[i]);
+            printf("%c", ciphertext[i]);
         }
-        printf("\n");
-
-
-
-        /*
-        for (int i = 0, n = strlen(plaintext); i < n; i++) {
-            int ciphertext = plaintext[i];
-            if (isalpha(ciphertext)) {
-                if (islower(ciphertext)) {
-                    //subtract 97 for alphabet ([0] - [25]) index
-                    ciphertext -= 97;
-                    ciphertext = (ciphertext + key) % 26;
-                    ciphertext +=  97;
-                }
-                else {
-                    //subtract 65 for alphabet ([0] - [25]) index
-                    ciphertext -= 65;
-                    ciphertext = (ciphertext + key) % 26;
-                    ciphertext +=  65;
-                }
-            }
-            printf("%c", (char)ciphertext);
-        }
-        */
         printf("\n");
 
     }
