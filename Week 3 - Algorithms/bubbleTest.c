5c0b1594304cf0547b5a51ee9b8dc4b341f7a09a
00saad <00saadchaudhry@gmail.com>
2017-12-17 18:17:56 -0500

    Basic bubble sort working, but needs more tweaking

--- Week 3 - Algorithms/bubbleTest.c
+++ Week 3 - Algorithms/bubbleTest.c
@@ -18,16 +18,19 @@ int main(int argc, string argv[])
         string word = argv[1];
         int n = strlen(argv[1]);
         int counter = 13;
+        int swap;
 
         for (int i = 0; i < n; i++)
         {
             counter = 0;
-            for (int j = 0; j < n; j++)
+            for (int j = 0; j < n - 1; j++)
             {
                 if (word[j] > word[j+1])
                 {
                     counter++;
+                    swap = word[j];
                     word[j] = word[j+1];
+                    word[j+1] = swap;
                     printf("%c", word[j]);
                 }
             }
