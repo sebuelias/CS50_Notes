5d1cc719a7d4d0642360aa6ce7859ed6588e75dd
00saad <00saadchaudhry@gmail.com>
2017-12-17 22:08:24 -0500

    Created bubble sort algorithm for helpers file

--- Week 3 - Algorithms/Pset3/find/helpers.c
+++ Week 3 - Algorithms/Pset3/find/helpers.c
@@ -36,16 +36,29 @@ bool search(int value, int values[], int n)
  */
 void sort(int values[], int n)
 {
-    // TODO: implement a sorting algorithm, selection sort or bubble sort O(n^2)
-    int counter = -1;
+    int counter = 13; //counter value a non-zero number, to get loop running properly
+    int swap; //temporary swap variable
+
 
     for (int i = 0; i < n; i++)
     {
-        counter = 0;
-        if (values[i] > values[i]+1)
+        counter = 0; //set to 0 for start of loop
+
+        for (int j = 0; j < n-1; j++)
         {
-            values[i]++;
-            counter++;
+            if (values[j] > values[j+1])
+            {
+                swap = values[j]; //temporarily assigns left-side index to variable
+                values[j] = values[j+1]; //switches left-side index with right-side
+                values[j+1] = swap; //assigns right-side index to temp variable
+
+                counter++; //increments whenever a swap is made
+            }
+        }
+        //indicates end of swapping, all elements are in order
+        if (counter == 0)
+        {
+            break;
         }
     }
 }
