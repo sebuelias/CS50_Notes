c109cae9958bfc6185718d27d2f8c20e7d5302de
00saad <00saadchaudhry@gmail.com>
2017-12-17 20:31:22 -0500

    Bubble test final draft program done, will move onto real program after

--- Week 3 - Algorithms/bubbleTest.c
+++ Week 3 - Algorithms/bubbleTest.c
@@ -18,24 +18,25 @@ int main(int argc, string argv[])
         string word = argv[1];
 
         int n = strlen(argv[1]);
-        int counter = 13;
-        char swap;
+        int counter = 13; //counter value a non-zero number, to get loop running properly
+        char swap; //temporary swap variable
 
         for (int i = 0; i < n; i++)
         {
-            counter = 0;
+            counter = 0; //set to 0 for start of loop
             for (int j = 0; j < n - 1; j++)
             {
                 if (word[j] > word[j+1])
                 {
-                    counter++;
-
                     swap = word[j]; //temporarily assigns left-side index to variable
                     word[j] = word[j+1]; //switches left-side index with right-side
                     word[j+1] = swap; //assigns right-side index to temp variable
+
+                    counter++; //increments whenever a swap is made
                 }
             }
 
+            //indicates end of swapping, all elements are in order
             if (counter == 0)
             {
                 break;
