57bd0c15dacdd3e5160d474489dbf6796a0612dc
00saad <00saadchaudhry@gmail.com>
2017-12-21 09:09:47 -0500

    Started binary search test program

---
+++ Week 3 - Algorithms/binaryTest.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, string argv[])
+{
+
+    if (argc != 2)
+    {
+        return 1;
+    }
+
+    else if (argc == 2)
+    {
+
+        
+    }
+
+return 0;
+}
