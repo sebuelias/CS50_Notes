3d54b40f6e9e14e6f428387d1fe3b94305cd69ee
00saad <00saadchaudhry@gmail.com>
2018-01-01 08:43:56 -0500

    Fixed float-to-int conversion in greedy program. Completed initials_more program

--- Week 1 - C/PSet1/greedy.c
+++ Week 1 - C/PSet1/greedy.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <math.h>
 
 int main(void) {
 
@@ -11,45 +12,36 @@ int main(void) {
     //number of all coins used
     int coin = 0;
 
-    //number of each coin type
-    int quarter = 0;
-    int dime = 0;
-    int nickel = 0;
-    int penny = 0;
-
     //make sure user enters appropriate value
     do {
         printf("Customer change: ");
         change = get_float();
         //convert change float to changeCoin int for accurate math
-        changeCoin = change * 100;
+        changeCoin = change * 1000;
+        changeCoin /= 10;
     } while(change < 0);
 
     //quarter
     while (changeCoin >= 25) {
         coin++;
-        quarter--;
         changeCoin -= 25;
     }
 
     //dime
     while (changeCoin >= 10 && changeCoin < 25) {
         coin++;
-        dime--;
         changeCoin -= 10;
     }
 
     //nickel
     while (changeCoin >= 5 && changeCoin < 10) {
         coin++;
-        nickel--;
         changeCoin -= 5;
     }
 
     //penny
     while (changeCoin > 0 && changeCoin < 5) {
         coin++;
-        penny--;
         changeCoin -= 1;
     }
 
